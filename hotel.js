7c3110d5579d573df62cf9e029c9d0426c80acf2
Diana Linville <dianar@nwmissouri.edu>
2019-02-13 13:45:25 -0600

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,24 @@
+/* The script is placed inside an immediately invoked function expression
+   which helps protect the scope of variables */
+
+//PART ONE: GENERATE A RANDOM ROOM RATE. Write a function called genRand() that has no paremeters that will return a room rate between 100 and 400. HINT: You should hit a number within this range within 10 tries.
+  
+
+
+
+
+  // PART TWO: CALCULATE AND RETURN THE EXPIRY DETAILS FOR THE OFFER. Write a function called offerExpires that has one parameter a Date object. The function should return a string in the format "OFFER EXPIRES NEXT "WeekDay" (Date Month Year)". The date should be one week from today's date
+
+  
+//PART THREE: CALL THE FUNCTIONS AND WRITE INFO TO WEBPAGE
+
+//Call getRand() function to get random room rate and write it to the special rate section on the webpage
+
+
+//create a new Date object
+
+
+//Call the function passing the date object to it and write it to the offer ends section on the webpage
+
+
+
